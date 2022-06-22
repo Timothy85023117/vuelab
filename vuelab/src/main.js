be677e78874de6f1acd1b7d4e4026fc09936093e
Timothy85023117 <s94kiwi9191@gmail.com>
2022-06-22 17:04:52 +0800

    structure

--- vuelab/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import 'owl.carousel/dist/assets/owl.carousel.css'
-import 'owl.carousel'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
--- vuelab/src/server/index.js
+++
@@ -1,5 +0,0 @@
-import products from './product.js'
-
-export {
-  products
-}
